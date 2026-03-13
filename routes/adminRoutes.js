0ffbe75a9bae6741e76c6c066cb4c5d1744579f8
Hubaib Siddiqui <hubaibsiddiqui123@gmail.com>
2026-03-14 02:49:26 +0530

    fix bcrypt module

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -111,7 +111,7 @@
 const express = require("express");
 const router = express.Router();
 const Admin = require("../models/Admin");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 // REGISTER ADMIN
