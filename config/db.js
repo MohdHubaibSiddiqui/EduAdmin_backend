ed44d8b2965d997e21b0b4365bf63d9cd9ce9b35
Hubaib Siddiqui <hubaibsiddiqui123@gmail.com>
2026-03-13 23:43:21 +0530

    eduadmin backend code added dotenv

--- config/db.js
+++ config/db.js
@@ -1,12 +1,26 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect("mongodb://127.0.0.1:27017/collegeDB");
+//     console.log("✅ MongoDB Connected");
+//   } catch (error) {
+//     console.log("❌ DB Error:", error);
+//   }
+// };
+
+// module.exports = connectDB;
+
+const dns=require("dns");
+//change dns
+dns.setServers(["1.1.1.1","8.8.8.8"]);
 const connectDB = async () => {
-  try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/collegeDB");
-    console.log("✅ MongoDB Connected");
-  } catch (error) {
-    console.log("❌ DB Error:", error);
-  }
-};
+require("dotenv").config();
+const mongoose=require("mongoose")
 
+mongoose.connect(process.env.MONGO_URI)
+
+.then(() => console.log("MongoDB Atlas Connected"))
+.catch((err) => console.log(err));
+}
 module.exports = connectDB;
